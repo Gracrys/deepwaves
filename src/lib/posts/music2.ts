fed22c2ab4b4434f74d46c43d70e76dd9db92e10
Gracrys <gracrys@gmail.com>
2025-09-08 21:29:09 -0500

    feat: music 2 article hwo to listen to music

---
+++ src/lib/posts/music2.ts
@@ -0,0 +1,21 @@
+const music2 = {
+	title: 'Digging deeper into music',
+	img: '/blog/music.jpg',
+	slug: 'music-enjoying',
+	summary: 'Digging deeper into music and refining',
+	tags: ['#music'],
+	md: `
+Welcome to part 2 of learning to listening to music, so, pleb, lets say you like top 50 pop and pop punk, the same green day, avril Lavigne, and one day, you listened, and even liked one ramones song, that song, nice! But you didn't like any other song, and you ask yourself why.
+
+What makes a song interesting, a release, and how are tastes made? Well, music tastes are based on personality, your generic tastes, stuff like if you enjoy chamomile tea you can enjoy lo fi (is a kinda complicated relationship im doing here so dont take it too hard lol), your musical tastes obv, and culture, woah, but is also a changing ecosystem.... just as music is in general.
+
+Now, your tastes get refined progressively, i will use the term refined but we can also say "expand" not only by discovering new things, but you start to understand what make x style appealing, maybe after checking all that punk you find you like the danceable part of punk, which is understandable, and not only that, but you can find a lot of that, and you go next to industrial and you like the Early punk style of it, those are some examples of how it may work, and while you are on it, you start to like stuff you didn't like before because you start to "understand" it, you get what's appealing on noise rock or power electronics, but it only will happen if you open your senses to it and give it an opportunity.
+
+In a more advanced notes, the same way we start to understand new styles, bands, etc, we may feel guilty how certain style that clicks with some of the vibe you are interested in doesn't hits you, and thats okay, most genres are really broad and can definite different styles, i will use nu metal as an example, where you have three styles (there are more but not gonna expand too much) the more alternative post grunge disturbed, drowning pool style which is more closer to normal metal, the harsh korn, static x style, using hooks, a more darker gritty approach and more aggressive, and the original deftones style full of screams and groans but also the back and forth between soft and hard melodies, every genre has its uniqueness, and it is normal that you wont like everything it has to offer.
+`
+	,
+	html: ''
+}
+
+
+export default music2
