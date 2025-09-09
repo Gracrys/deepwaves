e38a1610f085b0abb288c7bd96d5fe798506a8eb
Gracrys <gracrys@gmail.com>
2025-09-08 22:31:05 -0500

    feat: added article lifelover

---
+++ src/lib/posts/lifelover.ts
@@ -0,0 +1,21 @@
+const lifelover = {
+	title: 'Lifelover',
+	img: '/blog/lifelover.jpg',
+	slug: 'lifelover',
+	summary: 'playful and dark depressive metal from sweden',
+	tags: ['#music', '#dsbm', '#doom', '#metal'],
+	md: `
+lifelover
+
+Dissonant Dsbm with heavy nostalgic tones, starts with the classic suffering thematic in nackskot and finishes the song with a out of contrast children chant, love, pitch and black melody made me hit the album in awe, the harmony goes from fuzz and angst and waves into piano back and forth in a bittersweet tone.
+
+Stockholm incorporates a more softer sound with a doom melody and a whistle, making of a wavy ballad.
+
+The album in general embraces the noise, the fuzz and the classic dsbm suffering without staying repetitive or boring, being relaxing, introspective even comfy in a way.
+`
+	,
+	html: ''
+}
+
+
+export default lifelover
